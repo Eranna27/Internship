d5de0c04ff17090009a1d5a19bdc55a3c0c859d1
madugundueranna <meranna60@gmail.com>
2025-12-17 19:50:17 +0530

    ok

--- Config/Transporter.js
+++ Config/Transporter.js
@@ -1,21 +1,21 @@
-// require("dotenv").config();
-// const sgMail = require("@sendgrid/mail");
-// if (!process.env.SENDGRID_API_KEY) {
-//     throw new Error("SENDGRID_API_KEY is missing");
-// }
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-// module.exports = sgMail;
+require("dotenv").config();
+const sgMail = require("@sendgrid/mail");
+if (!process.env.SENDGRID_API_KEY) {
+    throw new Error("SENDGRID_API_KEY is missing");
+}
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+module.exports = sgMail;
 
-const nodeMailer = require("nodemailer");
+// const nodeMailer = require("nodemailer");
 
-const transporter = nodeMailer.createTransport({
-    host: "smtp.gmail.com", //  SMTP host (e.g., "smtp.gmail.com" for Gmail)
-    port: 587, //  SMTP port (e.g., 587 for Gmail)
-    secure: false,
-    auth: {
-        user: process.env.MAIL,
-        pass: process.env.PASS,
-    },
-});
+// const transporter = nodeMailer.createTransport({
+//     host: "smtp.gmail.com", //  SMTP host (e.g., "smtp.gmail.com" for Gmail)
+//     port: 587, //  SMTP port (e.g., 587 for Gmail)
+//     secure: false,
+//     auth: {
+//         user: process.env.MAIL,
+//         pass: process.env.PASS,
+//     },
+// });
 
-module.exports = transporter;
+// module.exports = transporter;
